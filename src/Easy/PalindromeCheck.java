6a9a8ca6b9a20b33499902ec77efa834d8c1756e
tej0897 <tej897@gmail.com>
2023-04-05 10:42:58 +0530

    leet problems on April 5th

---
+++ src/Easy/PalindromeCheck.java
@@ -0,0 +1,22 @@
+package Easy;
+
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        int palindromeCheck = 123;
+        int sum = 0;
+        int temp = palindromeCheck;
+        int rem;
+
+        while(palindromeCheck > 0){
+            rem = palindromeCheck % 10;
+            sum = (sum * 10) + rem;
+            palindromeCheck /= 10;
+        }
+
+        if (sum == temp){
+            System.out.println("Palindrome!");
+        } else {
+            System.out.println("Not a Palindrome!!");
+        }
+    }
+}
---
+++ src/Easy/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,41 @@
+package Easy;
+
+import java.util.Arrays;
+
+public class RemoveDuplicatesFromSortedArray {
+    public static void main(String[] args) {
+        int arr[] = {0,0,1,1,1,2,2,3,3,4};
+        int sortedArray [] =  Arrays.stream(arr).distinct().toArray();
+        for( int i=0; i< sortedArray.length; i++){
+            System.out.println(sortedArray[i]);
+        }
+    }
+}
+
+// check this one
+
+//class Solution {
+//    public int removeDuplicates(int[] nums) {
+//        int n = nums.length;
+//
+//        if (n==0 || n==1){
+//            return n;
+//        }
+//
+//        int[] temp = new int[n];
+//        int j = 0;
+//
+//        for (int i=0; i<n-1; i++){
+//            if (nums[i] != nums[i+1]){
+//                temp[j++] = nums[i];
+//            }
+//        }
+//        temp[j++] = nums[n-1];
+//        // Changing original array
+//        for (int i=0; i<j; i++){
+//            nums[i] = temp[i];
+//        }
+//        return j;
+//    }
+//}
+
