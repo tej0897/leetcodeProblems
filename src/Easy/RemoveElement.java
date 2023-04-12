1b39063e5144bfbbd3383f13e2ec784fbc1be293
tej0897 <tej897@gmail.com>
2023-04-12 19:24:46 +0530

    Remove Element program

---
+++ src/Easy/RemoveElement.java
@@ -0,0 +1,29 @@
+package Easy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class RemoveElement {
+    public static void main(String[] args) {
+        int [] intArray = {3,2,2,3};
+        int value = 2;
+        removeElement(intArray, value);
+    }
+
+    private static void removeElement(int[] intArray, int value) {
+
+        int count = 0;
+        // Loop through all the elements of the array
+        for (int i = 0; i < intArray.length; i++) {
+            // If the element is not val
+            if (intArray[i] != value) {
+                intArray[count++] = intArray[i];
+            }
+        }
+        System.out.println(count);
+        for (int j : intArray){
+            System.out.println(intArray[j]);
+        }
+    }
+
+}
