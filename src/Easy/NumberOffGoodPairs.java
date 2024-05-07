3b94da02107f4a86c1c04366aef6ba0dfef86700
tej0897 <tej0897@gmail.com>
2024-05-07 20:10:23 +0530

    NumberOffGoodPairs program

---
+++ src/Easy/NumberOffGoodPairs.java
@@ -0,0 +1,17 @@
+package Easy;
+
+public class NumberOffGoodPairs {
+    public static void main(String[] args) {
+        int [] nums = {1,2,3,1,1,3};
+        int output = 0;
+
+        for(int i=0; i< nums.length-1; i++){
+            for (int j=1; j < nums.length; j++){
+                if (nums[i] == nums[j] && i < j){
+                    output++;
+                }
+            }
+        }
+        System.out.println(output);
+    }
+}
