a82dc4bb073582e64e8d1d743f9b1f8eb870bf4b
tej0897 <tej0897@gmail.com>
2024-05-07 15:54:39 +0530

    Concatenation Of Array program

---
+++ src/Easy/ConcatenationOfArray.java
@@ -0,0 +1,21 @@
+package Easy;
+
+public class ConcatenationOfArray {
+    public static void main(String[] args) {
+
+        int [] input = {1,3,2,1};
+        int [] output = new int[input.length*2];
+//        for(int h : input){
+//            System.out.println(h);
+//        }
+
+        for(int i=0; i<input.length; i++){
+            output[i] = input[i];
+            output[i+ input.length] = input[i];
+        }
+
+        for(int h : output){
+            System.out.println(h);
+        }
+    }
+}
