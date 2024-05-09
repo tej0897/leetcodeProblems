cab2a0fdd617100a093e06ef49027d882b2c5b86
tej0897 <tej0897@gmail.com>
2024-05-09 09:43:20 +0530

    ArrayFromPermutation program

---
+++ src/Easy/ShuffleTheArray.java
@@ -0,0 +1,32 @@
+package Easy;
+
+public class ShuffleTheArray {
+    public static void main(String[] args) {
+
+        /**
+         * 0,2,4,   1,3,5
+         * 1,2,3,   4,5,6
+         * 0,1,2,   3,4,5
+         *
+         * half-point = 3
+         *
+         *
+         */
+
+
+        int [] nums = {2,5,1,3,4,7};
+        int [] array1 = new int[nums.length/2];
+        int [] array2 = new int[nums.length/2];
+
+
+
+
+
+        for (int j : array1){
+            System.out.println(j);
+        }
+        for (int j : array2){
+            System.out.println(j);
+        }
+    }
+}
