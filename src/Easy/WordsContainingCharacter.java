b4a4f3a75d57010d8de293b7abce9783b43c6e4d
tej0897 <tej0897@gmail.com>
2024-05-09 10:19:08 +0530

    WordsContainingCharacter program

---
+++ src/Easy/WordsContainingCharacter.java
@@ -0,0 +1,24 @@
+package Easy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class WordsContainingCharacter {
+    public static void main(String[] args) {
+        String [] input = {"leet","code"};
+        List<Integer> answer = functionCheck(input, 'e');
+        System.out.println(answer);
+    }
+    public static List<Integer> functionCheck(String [] input , char x ) {
+
+        List<Integer> output = new ArrayList<>();
+
+        for (int i = 0; i < input.length; i++) {
+            if (input[i].contains(String.valueOf(x))) {
+                output.add(i);
+            }
+        }
+        return output;
+    }
+}
