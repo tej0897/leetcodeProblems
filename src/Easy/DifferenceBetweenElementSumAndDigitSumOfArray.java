4ed96967665286988f0bc990b481067eab66880c
tej0897 <tej0897@gmail.com>
2024-05-15 21:42:01 +0530

    DifferenceBetweenElementSumAndDigitSumOfArray program

---
+++ src/Easy/DifferenceBetweenElementSumAndDigitSumOfArray.java
@@ -0,0 +1,24 @@
+package Easy;
+
+public class DifferenceBetweenElementSumAndDigitSumOfArray {
+
+
+    public static int differenceOfSum(int[] nums) {
+        int elementSum = 0;
+        int digitSum = 0;
+        int len = nums.length;
+        for (int i =0; i < len; i++){
+            elementSum += nums[i];
+            while (nums[i] > 0){
+                digitSum = digitSum + nums[i] % 10;
+                nums[i] /= 10;
+            }
+        }
+        return elementSum - digitSum;
+    }
+
+    public static void main(String[] args) {
+        int [] input = {1,15,6,3};
+        System.out.println(differenceOfSum(input));
+    }
+}
