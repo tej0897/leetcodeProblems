c4450235fe37ee8625f7e79805da4bb8dd87fdb0
tej0897 <tej897@gmail.com>
2024-05-19 13:33:29 +0530

    interview programs

--- src/interview_programs/FrequencyOfString.java
+++ src/interview_programs/FrequencyOfString.java
@@ -1,28 +1,24 @@
 package interview_programs;
 
+import java.util.HashMap;
+
 public class FrequencyOfString {
-    static final int SIZE = 26;
 
     public static void countFrequency(String str){
-        int n = str.length();
-        int [] frequency  = new int[SIZE];
-        for (int i=0; i < n; i++)
-            frequency[str.charAt(i) - 'a']++;
+        HashMap<Character, Integer> charFrequency = new HashMap<>();
 
-        for (int i = 0; i < n; i++) {
-            if (frequency[str.charAt(i) - 'a'] != 0) {
-
-                // print the character along with its
-                // frequency
-                System.out.print(str.charAt(i));
-                System.out.print(frequency[str.charAt(i) - 'a'] + " ");
-
-                // update frequency of str.charAt(i) to
-                // 0 so that the same character is not
-                // printed again
-                frequency[str.charAt(i) - 'a'] = 0;
+        for (char ch : str.toCharArray()){
+            if (charFrequency.containsKey(ch)) {
+                charFrequency.put(ch, charFrequency.get(ch) +1);
+            } else {
+                charFrequency.put(ch, 1);
             }
         }
+        System.out.println("Character frequencies:");
+        for (char ch : charFrequency.keySet()) {
+            System.out.println(ch + ": " + charFrequency.get(ch));
+        }
+
     }
 
     public static void main(String[] args) {
