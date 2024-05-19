8d7a2e83c7f5dfe45178fac4d0334267c32f3da6
tej0897 <tej897@gmail.com>
2024-05-19 13:28:39 +0530

    interview programs

--- src/interview_programs/PrintInAscOrder.java
+++ src/interview_programs/PrintInAscOrder.java
@@ -9,9 +9,19 @@ public class PrintInAscOrder {
         System.out.println("Enter a number: ");
         int n = scanner.nextInt();
         int temp = 0;
-        for (int i=0; i<n; i++){
-            temp = i+1+(temp*10);
+        for (int i=1; i<=n; i++){
+            temp = i+(temp*10);
             System.out.println(temp);
         }
+        /*
+         * Alternate solution using 2 for loops
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+         */
     }
 }
---
+++ src/interview_programs/PrintInDescOrder.java
@@ -0,0 +1,13 @@
+package interview_programs;
+
+public class PrintInDescOrder {
+    public static void main(String[] args) {
+        int n=3;
+        for (int i = n; i >= 1; i--){
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
