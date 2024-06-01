0f213cfc682207327025fadf47e5765e2ca655cb
tej0897 <tej897@gmail.com>
2024-06-01 18:57:49 +0530

    interview questions commit

---
+++ src/interview_programs/MeetTargetFromArrayPair.java
@@ -0,0 +1,22 @@
+package interview_programs;
+
+import java.util.HashSet;
+
+public class MeetTargetFromArrayPair {
+    public static void main(String[] args) {
+        int [] intArray = {1,2,3,4,5,6,7,8};
+        int target = 5;
+        printPairs(intArray, target);
+    }
+
+    private static void printPairs(int[] intArray, int target) {
+        HashSet<Integer> hashSet = new HashSet<>();
+        for (int num : intArray){
+            int compliment = target - num;
+            if (hashSet.contains(compliment)){
+                System.out.println("("+num+", "+compliment+")");
+            }
+            hashSet.add(num);
+        }
+    }
+}
